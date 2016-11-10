07ef78726440a01ce9f3fae8a0f6181f08ec5c08
thomasgriffinx <xytomg@gmail.com>
2016-11-10 11:59:50 +0000

    image fix

--- index.php
+++ index.php
@@ -12,7 +12,12 @@
 	
 	<!-- Viewport -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+	
+<style>
+img {
+    image-orientation: from-image;
+}
+</style>
 </head>
 <body>
 	<!-- Header with title-->
