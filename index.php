509489858a3ff9c56868ff7e26f1b66f2068a596
thomasgriffinx <xytomg@gmail.com>
2016-11-10 10:19:35 +0000

    master

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Completed Floors by FSC Merseyside</title>
+<!-- Behavioral Meta Data -->
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="icon" type="image/png" href="img/small-logo-01.png">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,900,900italic,700italic,700,500italic,400italic,500,300italic,300' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+	<link href='style.css' rel='stylesheet' type='text/css'>
+	</head>
+
+<body>
+
+<a name="ancre"></a>
+
+<!-- CACHE -->
+<div class="cache"></div>
+
+<!-- HEADER -->
+
+<div id="wrapper-header">
+	<div id="main-header" class="object">
+		<div class="logo"><img src="img/logo.png" alt="logo fsc" height="38" width="90"></div>
+        <div id="stripes"></div>
+    </div>
+</div>
+
+<!-- NAVBAR -->
+
+	<div id="wrapper-navbar">
+		<div class="navbar object">
+    		<div id="wrapper-sorting">
+            <div id="wrapper-title-1">
+            <div class="top-rated object"><a href ="http://flooringsupplycentre.co.uk/index.php?Page=1">Visit our full site</a></div>
+            	<div id="fleche-nav-1"></div>
+    		</div>
+            
+            </div>
+           
+
+<!--HEADER-->
+
+
+<!-- PORTFOLIO -->
+
+	<div id="wrapper-container">
+    
+		<div class="container object">
+
+			<div id="main-container-image">
+                       
+					<section class="work">
+
+						<figure class="white">
+							<a href="#">
+								<img src="img/c1.jpg" alt="" />
+								<dl>
+									<dt>Supply & Fit @ FSC</dt>
+									<dd> Call us direct for the best prices and service in the UK 0151 521 5221</dd>	
+								</dl>
+							</a>
+                            <div id="wrapper-part-info">
+                            	<div id="part-info"></div>
+							</div>
+                        </figure>	
+
+						<figure class="white">
+							<a href="#">
+								<img src="img/c2.jpg" alt="" />
+								<dl>
+									<dt>Supply & Fit @ FSC</dt>
+									<dd>Call us direct for the best prices and service in the UK 0151 521 5221</dd>	
+								</dl>
+							</a>	
+                            <div id="wrapper-part-info">
+                            
+                            	<div id="part-info"></div>
+							</div>
+						</figure>	
+
+						<figure class="white">
+							<a href="#">
+								<img src="img/c3.jpg" alt="" />
+								<dl>
+									<dt>Supply & Fit @ FSC</dt>
+									<dd>Call us direct for the best prices and service in the UK 0151 521 5221</dd>	
+								</dl>
+							</a>
+                            <div id="wrapper-part-info">
+                         
+                            	<div id="part-info"></div>
+							</div>
+						</figure>	
+
+							<figure class="white">
+							<a href="#">
+								<img src="img/q4.jpg" alt="" />
+								<dl>
+									<dt>Supply & Fit @ FSC</dt>
+									<dd>Call us direct for the best prices and service in the UK 0151 521 5221</dd>	
+								</dl>
+							</a>
+                            <div id="wrapper-part-info">
+                         
+                            	<div id="part-info"> </div>
+							</div>
+						</figure>	
+
+										<figure class="white">
+							<a href="#">
+								<img src="img/c1.jpg" alt="" />
+								<dl>
+									<dt>Supply & Fit @ FSC</dt>
+									<dd> Call us direct for the best prices and service in the UK 0151 521 5221</dd>	
+								</dl>
+							</a>
+                            <div id="wrapper-part-info">
+                            	<div id="part-info"></div>
+							</div>
+                        </figure>	
+
+						<figure class="white">
+							<a href="#">
+								<img src="img/c2.jpg" alt="" />
+								<dl>
+									<dt>Supply & Fit @ FSC</dt>
+									<dd>Call us direct for the best prices and service in the UK 0151 521 5221</dd>	
+								</dl>
+							</a>	
+                            <div id="wrapper-part-info">
+                            
+                            	<div id="part-info"></div>
+							</div>
+						</figure>	
+
+						<figure class="white">
+							<a href="#">
+								<img src="img/c3.jpg" alt="" />
+								<dl>
+									<dt>Supply & Fit @ FSC</dt>
+									<dd>Call us direct for the best prices and service in the UK 0151 521 5221</dd>	
+								</dl>
+							</a>
+                            <div id="wrapper-part-info">
+                         
+                            	<div id="part-info"></div>
+							</div>
+						</figure>	
+
+							<figure class="white">
+							<a href="#">
+								<img src="img/q4.jpg" alt="" />
+								<dl>
+									<dt>Supply & Fit @ FSC</dt>
+									<dd>Call us direct for the best prices and service in the UK 0151 521 5221</dd>	
+								</dl>
+							</a>
+                            <div id="wrapper-part-info">
+                         
+                            	<div id="part-info"> </div>
+							</div>
+						</figure>
+						
+					</section>
+                    
+				</div>	
+            	    
+			</div>
+
+      
+
+    
+
+
+
+
+<!-- SCRIPT -->
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.scrollTo.min.js"></script>
+    <script type="text/javascript" src="js/jquery.localScroll.min.js"></script>
+    <script type="text/javascript" src="js/jquery-animate-css-rotate-scale.js"></script>
+	<script type="text/javascript" src="js/fastclick.min.js"></script>
+	<script type="text/javascript" src="js/jquery.animate-colors-min.js"></script>
+	<script type="text/javascript" src="js/jquery.animate-shadow-min.js"></script>    
+	<script type="text/javascript" src="js/main.js"></script>
+	<script>
+
+/* PRELOADER */
+
+function preloader() {
+	if (document.images) {
+		var img1 = new Image();
+		var img2 = new Image();
+		var img3 = new Image();
+		var img4 = new Image();
+		var img5 = new Image();
+		var img6 = new Image();
+		var img7 = new Image();
+		var img8 = new Image();
+		var img9 = new Image();
+		var img10 = new Image();
+		var img11 = new Image();
+		var img12 = new Image();
+		var img13 = new Image();
+		var img14 = new Image();
+		var img15 = new Image();
+		var img16 = new Image();
+		var img17 = new Image();
+		var img18 = new Image();
+		var img19 = new Image();
+		var img20 = new Image();
+
+		img1.src = "img/psd-4.jpg";
+		img2.src = "img/font-1.jpg";
+		img3.src = "img/psd-1.jpg";
+		img4.src = "img/psd-2.jpg";
+		img5.src = "img/ai-1.jpg";
+		img6.src = "img/theme-2.jpg";
+		img7.src = "img/psd-3.jpg";
+		img8.src = "img/font-2.jpg";
+		img9.src = "img/font-3.jpg";
+		img10.src = "img/ai-2.jpg";
+		img11.src = "img/icons-1.jpg";
+		img12.src = "img/ui-1.jpg";
+		img13.src = "img/font-5.jpg";
+		img14.src = "img/theme-2.jpg";
+		img15.src = "img/psd-5.jpg";
+		img16.src = "img/icons-3.jpg";
+		img17.src = "img/font-4.jpg";
+		img18.src = "img/theme-3.jpg";
+		img19.src = "img/font-6.jpg";
+		img20.src = "img/theme-4.jpg";
+	}
+}
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			if (oldonload) {
+				oldonload();
+			}
+			func();
+		}
+	}
+}
+addLoadEvent(preloader);
+
+
+
+</script>
+
+
+</body>
+
+
+</html>
