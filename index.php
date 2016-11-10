0fbb7b0fc73b2aa03b40ff524d582bef5d56b97e
thomasgriffinx <xytomg@gmail.com>
2016-11-10 11:21:26 +0000

    links

--- index.php
+++ index.php
@@ -18,32 +18,11 @@
 	<!-- Header with title-->
 	<header>
 		<div class="wrapper">
-			<h1>InContent</h1>
-			<p>Image content with description building with CSS3 and LESS/SASS.</p>
+			<h1>Installed by Flooring Supply Centre</h1>
+			<p>Visit our <a href ="http://flooringsupplycentre.co.uk/index.php?Page=1">full site </a> for more information or call us directly on 0151 521 5221</p>
 		</div>
 	</header>
 	
-	<!-- Begin of Effects -->
-
-	<section class="wrapper cl">
-		<!--Effect: Bottom to Top -->
-		<div class="pic">
-		    <img src="img/0.jpeg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption bottom-to-top">
-		        <h1 class="pic-title">Bottom to Top</h1>
-		        <p>Hi, this is a simple example =D</p>
-		    </span>
-		</div>
-
-		<!--Effect: Top to Bottom -->
-		<div class="pic">
-		    <img src="img/0-2.jpeg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption top-to-bottom">
-		        <h1 class="pic-title">Top to Bottom</h1>
-		        <p>Hi, this is a simple example =D</p>
-		    </span>
-		</div>
-		<!--Effect: Left to Right -->
 		<div class="pic">
 		    <img src="img/0-3.jpeg" class="pic-image" alt="Pic"/>
 		    <span class="pic-caption left-to-right">
@@ -52,90 +31,23 @@
 		    </span>
 		</div>
 
-		<!--Effect: Right to Left -->
-		<div class="pic">
-		    <img src="img/04.jpg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption right-to-left">
-		        <h1 class="pic-title">Right to Left</h1>
+			<div class="pic">
+		    <img src="img/0-2.jpeg" class="pic-image" alt="Pic"/>
+		    <span class="pic-caption left-to-right">
+		        <h1 class="pic-title">Left to Right</h1>
 		        <p>Hi, this is a simple example =D</p>
 		    </span>
 		</div>
 
-		<!--Effect: Rotate More -->
-		<div class="pic">
-		    <img src="img/05.jpg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption rotate-in">
-		        <h1 class="pic-title">Rotate In</h1>
-		        <p>Hi, this is a simple example =D</p>
-		    </span>
-		</div>
-
-		<!--Effect: Rotate LESS -->
-		<div class="pic">
-		    <img src="img/06.jpg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption rotate-out">
-		        <h1 class="pic-title">Rotate Out</h1>
-		        <p>Hi, this is a simple example =D</p>
-		    </span>
-		</div>
-
-		<!--Effect: Open Up -->
-		<div class="pic pic-3d">
-		    <img src="img/07.jpg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption open-up">
-		        <h1 class="pic-title">Open Up</h1>
-		        <p>Hi, this is a simple example =D</p>
-		    </span>
-		</div>
-
-		<!--Effect: Open Down -->
-		<div class="pic pic-3d">
-		    <img src="img/08.jpg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption open-down">
-		        <h1 class="pic-title">Open Down</h1>
-		        <p>Hi, this is a simple example =D</p>
-		    </span>
-		</div>
-
-		<!--Effect: Open Left -->
-		<div class="pic pic-3d">
-		    <img src="img/09.jpg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption open-left">
-		        <h1 class="pic-title">Open Left</h1>
-		        <p>Hi, this is a simple example =D</p>
-		    </span>
-		</div>
-
-		<!--Effect: Open Right -->
-		<div class="pic pic-3d">
-		    <img src="img/10.jpg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption open-right">
-		        <h1 class="pic-title">Open Right</h1>
-		        <p>Hi, this is a simple example =D</p>
-		    </span>
-		</div>
-
-		<!--Effect: Come Left -->
-		<div class="pic pic-3d">
-		    <img src="img/11.jpg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption come-left">
-		        <h1 class="pic-title">Come Left</h1>
-		        <p>Hi, this is a simple example =D</p>
-		    </span>
-		</div>
-
-		<!--Effect: Come Right -->
-		<div class="pic pic-3d">
-		    <img src="img/12.jpg" class="pic-image" alt="Pic"/>
-		    <span class="pic-caption come-right">
-		        <h1 class="pic-title">Come Right</h1>
+			<div class="pic">
+		    <img src="img/0.jpeg" class="pic-image" alt="Pic"/>
+		    <span class="pic-caption left-to-right">
+		        <h1 class="pic-title">Left to Right</h1>
 		        <p>Hi, this is a simple example =D</p>
 		    </span>
 		</div>
 
 		
-	</section>
-	<!-- End of effects -->
 
 	<footer>
 		<section class="wrapper">
@@ -143,7 +55,7 @@
 				
 			</span>
 			<p>
-				Full Site @ <a href="http://bit.ly/BrunoTw" title="Flooring Supply Centre">Flooring Supply Centre.</a>
+				Full Site @ <a href="http://flooringsupplycentre.co.uk/index.php?Page=1" title="Flooring Supply Centre">Flooring Supply Centre.</a>
 			</p>
 
 		</section>
